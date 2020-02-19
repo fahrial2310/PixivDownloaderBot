692bb0c4a6942626f64b2539dc05e0f520c4ca33
root <root@l1.nachtalb.io>
2020-02-20 01:00:47 +0200

    Update example settings

--- pixivdownloader/bot/settings.example.py
+++ pixivdownloader/bot/settings.example.py
@@ -1,11 +1,14 @@
 import logging
 
 TELEGRAM_API_TOKEN = ''
+BASE_URL =  f'https://some.url/{TELEGRAM_API_TOKEN}'
 
 ADMINS = ['@USERNAME']
 
 PIXIV_USERNAME = ''
 PIXIV_PASSWORD = ''
+URL = BASE_URL + '/media'
+DOWNLOAD_TO = '/some/path/to/files'
 
 # More information about polling and webhooks can be found here:
 # https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks
@@ -15,7 +18,7 @@ MODE = {
     #     'listen': '127.0.0.1',
     #     'port': 5000,
     #     'url_path': TELEGRAM_API_TOKEN,
-    #     'url': 'https://your_domain.tld/%s' % TELEGRAM_API_TOKEN,
+    #     'url': BASE_URL
     # },
 }
 
